545dbd09744beead133c279f29d598f3d9118293
muetzelrya <muetzelrya@urbandaleschools.com>
2021-03-10 13:40:13 -0600

    12.0 and 12.1 Complete
    box go bounce

--- 12.0_Jedi_Training.py
+++ 12.0_Jedi_Training.py
@@ -1,28 +1,28 @@
 '''
-Sign your name:________________
+Sign your name:_____Ryan Muetzel_______
  
 All questions are about the final code in Chapter 12:
 
 1.) Where is the ball's original location?
-
+        (320, 240)
 2.) What are the variables dx and dy?
-
+        velocity in the x and y directions
 3.) How many pixels/sec does the ball move in the x-direction?
-
+        180
 4.) How many pixels/sec does the ball move in the y-direction?
-
+        -120
 5.) Which method is run 60 times/second?
-
+        on_update and on_draw
 6.) What does this code do?   self.dx *= -1
-
+        inverses the direction of the ball (in the x direction)
 7.) What does this code do?  self.pos_y += self.dy
-
+        it moves the ball in the y direction
 8.) What is the width of the window?
-
+        640
 9.) What is this code checking?  self.pos_y > SH - self.rad:
-
+        if the ball has hit the top of the window
 10.) What is this code checking? if self.pos_x < self.rad
-
+        if the ball has hit the left border of the window
 
 
 '''
--- 12.1_Thirty_Box_Bounce.py
+++ 12.1_Thirty_Box_Bounce.py
@@ -22,3 +22,83 @@ Helpful Hints:
 4.) Also in the on_draw section draw the side rails.
 5.) In the on_update section use: for box in self.boxlist: box.update_box()
 '''
+
+import arcade
+import random
+
+screen_height = 600
+screen_width = 600
+
+
+class Box:
+    def __init__(self, x, y, side, speed_x, speed_y):
+        self.x = x
+        self.y = y
+        self.wx = side
+        self.wy = side
+        self.color = arcade.color.BLACK
+        self.speed_x = speed_x
+        self.speed_y = speed_y
+        if self.speed_x == 0:
+            self.speed_x = 1
+        elif self.speed_y == 0:
+            self.speed_y = 1
+
+    def box_draw(self):
+        arcade.draw_rectangle_filled(self.x, self.y, self.wx, self.wy, self.color)  # draw each box
+        # draw the walls
+        arcade.draw_rectangle_filled(15, screen_height / 2, 30, screen_height - 60, arcade.color.BLUE)
+        arcade.draw_rectangle_filled(screen_width / 2, 15, screen_width - 60, 30, arcade.color.LIME_GREEN)
+        arcade.draw_rectangle_filled(screen_width - 15, screen_height / 2, 30, screen_height - 60, arcade.color.YELLOW)
+        arcade.draw_rectangle_filled(screen_width / 2, screen_height - 15, screen_width - 60, 30, arcade.color.RED)
+
+    def update_box(self):
+        self.x += self.speed_x
+        self.y += self.speed_y
+        # check if the thing is colliding with something
+        if self.x - (0.5 * self.wx) <= 30:  # collide with left wall
+            if self.speed_x < 0:
+                self.speed_x *= -1
+            self.color = arcade.color.BLUE
+        elif self.x + (0.5 * self.wx) >= screen_width - 30:     # collide with right wall
+            if self.speed_x > 0:
+                self.speed_x *= -1
+            self.color = arcade.color.YELLOW
+        elif self.y - (0.5 * self.wy) <= 30:    # collide with bottom wall
+            if self.speed_y < 0:
+                self.speed_y *= -1
+            self.color = arcade.color.LIME_GREEN
+        elif self.y + (0.5 * self.wy) >= screen_height - 30:        # collide with top wall
+            if self.speed_y > 0:
+                self.speed_y *= -1
+            self.color = arcade.color.RED
+
+
+class Render(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.WHITE)
+
+        self.box_list = []      # define the box list and add the boxes inside of the list
+        for i in range(30):
+            self.box_list.append(Box(random.randint(100, 500), random.randint(100, 500), random.randint(10, 50),
+                                     random.randint(-10, 10), random.randint(-10, 10)))
+        # self.box_1 = Box(100, 200, 25, 2, 2)
+
+    def on_draw(self):
+        arcade.start_render()
+        for i in range(len(self.box_list)):     # for every box in the list
+            self.box_list[i].box_draw()         # draw each box
+
+    def on_update(self, delta_time: float):
+        for i in range(len(self.box_list)):     # for every box in the list
+            self.box_list[i].update_box()       # update each box
+
+
+def main():
+    window = Render(screen_height, screen_width, '30 Boxes')
+    arcade.run()
+
+
+if __name__ == '__main__':
+    main()
